29afcd8b549af926a64e7759e6bfa57a0b6b4ed3
hftang <617775384@qq.com>
2020-08-13 16:36:48 +0800

    dev分支提交

--- main/java/com/example/demo/test/SecondClass.java
+++ main/java/com/example/demo/test/SecondClass.java
@@ -4,4 +4,30 @@ public class SecondClass {
 
     private String kkk;
     private String llll;
+
+    public SecondClass() {
+    }
+
+    public SecondClass(String kkk, String llll) {
+        this.kkk = kkk;
+        this.llll = llll;
+    }
+
+    public String getKkk() {
+        return kkk;
+    }
+
+    public void setKkk(String kkk) {
+        this.kkk = kkk;
+    }
+
+    public String getLlll() {
+        return llll;
+    }
+
+    public void setLlll(String llll) {
+        this.llll = llll;
+    }
+
+
 }
